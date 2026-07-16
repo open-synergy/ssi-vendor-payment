c3c5efce531d44bd40f94e83659579e8902cdbb6
Andhitia Rama <andhitia.r@gmail.com>
2026-07-16 21:39:44 +0700

    [ADD] ssi_vendor_payment_operating_unit
    Glue module yang menambahkan dukungan Operating Unit pada model
    vendor_payment. Menyediakan field operating_unit_id di view tree/search/form
    serta record rule yang menyaring visibilitas vendor payment per Operating Unit
    milik user. Field operating_unit_id berasal dari account.move (via
    ssi_financial_accounting_operating_unit) dan ter-delegasi lewat move_id,
    sehingga tidak ada kolom baru di tabel account_payment. Termasuk unit test
    skenario YAML (odoo-yaml-test).

---
+++ ssi_vendor_payment_operating_unit/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Vendor Payment + Operating Unit",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "contributors": [
+        "Andhitia Rama <andhitia.r@gmail.com>",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "ssi_vendor_payment",
+        "ssi_financial_accounting_operating_unit",
+    ],
+    "data": [
+        "security/ir_rule/vendor_payment.xml",
+        "views/vendor_payment_views.xml",
+    ],
+    "demo": [],
+}
